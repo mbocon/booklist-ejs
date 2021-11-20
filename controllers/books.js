ce9781fa178da0587398b99784720acd2b1393b1
Daniel JS <daniel@mydeveloperjourney.com>
2021-11-20 13:57:11 -0600

    wip: add latest feature/functionality

---
+++ controllers/books.js
@@ -0,0 +1,113 @@
+// require dependencies
+const express = require('express');
+// create a router object
+const booksRouter = express.Router();
+
+const Book = require('../models/book');
+// list our router actions
+
+// Seed route
+
+booksRouter.get('/books/seed', async (req, res) => {
+    const data = [
+        {
+            title: 'The Art of War',
+            author: 'Sun Tzu'
+        },
+        {
+            title: 'How to Win Friends and Influence People',
+            author: 'Dale Carnegie'
+        },
+        {
+            title: 'Think & Grow Rich',
+            author: 'Napolean Hill'
+        },
+        {
+            title: 'Rich Dad Poor Dad',
+            author: 'Robert Kiyosaki'
+        },
+    ];
+    await Book.deleteMany({});
+    await Book.create(data);
+    res.redirect('/books');
+});
+
+
+booksRouter.get('/destroy-data', async (req, res) => {
+    await Book.deleteMany({});
+    res.redirect('/books');
+});
+
+
+// Index Route
+booksRouter.get('/books', (req, res) => {
+    Book.find({}, (err, books) => {
+        res.render('index.ejs', { books });
+    });
+});
+
+
+// New Route
+booksRouter.get('/books/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+
+// Delete Route
+
+booksRouter.delete('/books/:id', (req, res) => {
+    Book.findByIdAndRemove(req.params.id, (err, deletedBook) => {
+        res.redirect('/books');
+    });
+});
+
+// Update route
+
+booksRouter.put('/books/:id', (req, res) => {
+    req.body.completed = !!req.body.completed; // !!'on' === true || !!undefined === false
+    Book.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        { new: true },
+        (err, book) => {
+          res.redirect(`/books/${req.params.id}`)
+    });
+});
+
+
+// Create Route
+booksRouter.post('/books', (req, res) => {
+    if(req.body.completed === 'on') {
+        req.body.completed = true;
+    } else {
+        req.body.completed = false;
+    }
+    Book.create(req.body, (err, book) => {
+        res.redirect('/books');
+    });
+});
+
+
+// Edit route
+
+booksRouter.get('/books/:id/edit', (req, res) => {
+    Book.findById(req.params.id, (err, book) => {
+        res.render('edit.ejs', { book });
+    });
+});
+
+
+// Show route
+
+booksRouter.get('/books/:id', (req, res) => {
+    // Book.findById(req.params.id).then(() => {
+    //     res.render('show.ejs', { book })
+    // });
+
+    Book.findById(req.params.id, (err, book) => {
+        res.render('show.ejs', { book });
+    });
+});
+// export the router object so that we require it in server.js
+
+module.exports = booksRouter;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // require deps
 const express = require('express');
 const mongoose = require('mongoose');
-const Book = require('./models/book');
 const methodOverride = require('method-override');
-
+const booksRouter = require('./controllers/books');
 // initialize app
 const app = express();
 
@@ -26,108 +25,7 @@ app.use(express.urlencoded({ extended: false })); // creates req.body
 app.use(methodOverride('_method'));
 // mount routes
 
-// Seed route
 
-app.get('/books/seed', async (req, res) => {
-    const data = [
-        {
-            title: 'The Art of War',
-            author: 'Sun Tzu'
-        },
-        {
-            title: 'How to Win Friends and Influence People',
-            author: 'Dale Carnegie'
-        },
-        {
-            title: 'Think & Grow Rich',
-            author: 'Napolean Hill'
-        },
-        {
-            title: 'Rich Dad Poor Dad',
-            author: 'Robert Kiyosaki'
-        },
-    ];
-    await Book.deleteMany({});
-    await Book.create(data);
-    res.redirect('/books');
-});
-
-
-app.get('/destroy-data', async (req, res) => {
-    await Book.deleteMany({});
-    res.redirect('/books');
-});
-
-
-// Index Route
-app.get('/books', (req, res) => {
-    Book.find({}, (err, books) => {
-        res.render('index.ejs', { books });
-    });
-});
-
-
-// New Route
-app.get('/books/new', (req, res) => {
-    res.render('new.ejs');
-});
-
-
-// Delete Route
-
-app.delete('/books/:id', (req, res) => {
-    Book.findByIdAndRemove(req.params.id, (err, deletedBook) => {
-        res.redirect('/books');
-    });
-});
-
-// Update route
-
-app.put('/books/:id', (req, res) => {
-    req.body.completed = !!req.body.completed; // !!'on' === true || !!undefined === false
-    Book.findByIdAndUpdate(
-        req.params.id, 
-        req.body, 
-        { new: true },
-        (err, book) => {
-          res.redirect(`/books/${req.params.id}`)
-    });
-});
-
-
-// Create Route
-app.post('/books', (req, res) => {
-    if(req.body.completed === 'on') {
-        req.body.completed = true;
-    } else {
-        req.body.completed = false;
-    }
-    Book.create(req.body, (err, book) => {
-        res.redirect('/books');
-    });
-});
-
-
-// Edit route
-
-app.get('/books/:id/edit', (req, res) => {
-    Book.findById(req.params.id, (err, book) => {
-        res.render('edit.ejs', { book });
-    });
-});
-
-
-// Show route
-
-app.get('/books/:id', (req, res) => {
-    // Book.findById(req.params.id).then(() => {
-    //     res.render('show.ejs', { book })
-    // });
-
-    Book.findById(req.params.id, (err, book) => {
-        res.render('show.ejs', { book });
-    });
-});
 
 // tell the app to listen
 
