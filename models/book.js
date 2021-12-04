74b05c08b9ea3f7f7d9ae39b11ff216408c0de9b
Daniel JS <daniel@mydeveloperjourney.com>
2021-12-04 16:49:22 -0600

    wip

--- models/book.js
+++ models/book.js
@@ -3,11 +3,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // define the schema
+const reviewSchema = new Schema({
+    text: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        default: 5
+    }
+}, { timestamps: true });
+
+
 const bookSchema = new Schema({
     title: { type: String, required: true, lowercase: true },
     author: { type: String, required: true, lowercase: true },
     completed: Boolean,
-    qty: { type: Number, default: 5}
+    qty: { type: Number, default: 5},
+    reviews: [reviewSchema] // this results in us being able to insert documents in this array
 }, { timestamps: true });
 
 // export the model to be accessed in server.js
